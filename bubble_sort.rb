1871c3df1b6ae14fec11f39c6740aab399e4ef3c
KossySteve <kossyeze@gmail.com>
2020-04-07 19:22:35 +0100

    fixed linter

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,35 +1,34 @@
-#Method definition
-def bubblesort(array) 
-   swapped = true
-   n = array.length-1
-   while swapped do
-       i = 0
-       swapped = false
-       while i < n do
-           if array[i] > array[i+1]
-            array[i],array[i+1] = array[i+1],array[i]
-            swapped = true
-           end
-           i+=1
-       end
-   end
-   array
+# Method definition
+def bubblesort(array)
+  swapped = true
+  n = array.length - 1
+  while swapped
+    i = 0
+    swapped = false
+    while i < n
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+      i += 1
+    end
+  end
+  array
 end
-            
 
 def bubble_sort_by(array)
-    swapped = true
-    n = array.length-1 
-    while swapped do
-        i = 0
-        swapped = false
-        while i < n do
-            if yield(array[i],array[i+1]) > 0
-                array[i],array[i+1] = array[i+1],array[i]
-                swapped = true
-            end
-            i+=1
-        end
+  swapped = true
+  n = array.length - 1
+  while swapped
+    i = 0
+    swapped = false
+    while i < n
+      if yield(array[i], array[i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+      i += 1
     end
-    array
+  end
+  array
 end
