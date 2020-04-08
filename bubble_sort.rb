5d409396b45feb35e8bf75d5ef10508b87de27d9
KossySteve <kossyeze@gmail.com>
2020-04-09 00:01:48 +0100

    correct error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,3 @@
-#Method definition
-def bubble_sort(array) 
-   swapped = true
-   n = array.length-1
-   while swapped do
-       i = 0
-       swapped = false
-       while i < n do
-           if array[i] > array[i+1]
-            array[i],array[i+1] = array[i+1],array[i]
-            swapped = true
-           end
-           i+=1
-       end
-   end
-   array
 # Method definition
 def bubblesort(array)
   swapped = true
@@ -29,7 +13,7 @@ def bubblesort(array)
       i += 1
     end
   end
-  array
+  print array
 end
 
 def bubble_sort_by(array)
@@ -46,5 +30,5 @@ def bubble_sort_by(array)
       i += 1
     end
   end
-  array
+  print array
 end
